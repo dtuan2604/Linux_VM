f010df53466ff16d01537eab811372389e0d7032
tdhhxn <tdhhxn@umsystem.edu>
2021-12-09 22:45:55 -0600

    Implement fifo algorithm

--- oss.c
+++ oss.c
@@ -23,6 +23,9 @@ static struct sembuf sema_operation;
 static int pcbt_shmid = -1;
 static ProcessControlBlock *pcbt_shmptr = NULL;
 
+static int fork_number = 0;
+static pid_t pid = -1;
+
 //invoke semaphore lock 
 static void ossSemWait(int sem_index)
 {
@@ -123,7 +126,80 @@ static void timer(int seconds)
 		perror("ERROR");
 	}
 }
+//interrupt handling
+static void masterHandler(int signum)
+{
+	fprintf(stderr,"%s: got interrupted. Finishing report and exiting...\n",prog);
+	
+	killAllChild();
 
+	double avgMem = (double)memoryaccess_number / (double)shmclock_shmptr->second;
+	double avgPagefault = (double)pagefault_number / (double)memoryaccess_number;
+	double avgSpeed = (double)total_access_time / (double)memoryaccess_number;
+	avgSpeed /= 1000000.0;
+
+	printWrite(fptr,"**************************************************************************\n");
+	printWrite(fptr,"\t\tOSS REPORT\n");
+	printWrite(fptr, "\tNumber of processes during this execution: %d\n", fork_number);
+	printWrite(fptr, "\tNumber of memory accesses: %d\n", memoryaccess_number);
+	printWrite(fptr, "\tNumber of page faults: %d\n", pagefault_number);
+	printWrite(fptr, "\tNumber of memory accesses per second: %f accesses/sec\n", avgMem);
+	printWrite(fptr, "\tNumber of page faults per memory access: %f page faults/access\n", avgPagefault);
+	printWrite(fptr, "\tAverage memory access speed: %f ms/access\n", avgSpeed);
+	printWrite(fptr,"**************************************************************************\n");
+
+	cleanUp();
+
+	//Final check for closing log file
+	if(fptr != NULL)
+	{
+		fclose(fptr);
+		fptr = NULL;
+	}
+
+	exit(EXIT_SUCCESS); 
+}
+static void segHandler(int signum)
+{
+	fprintf(stderr, "Segmentation Fault\n");
+	masterHandler(0);
+}
+static void exitHandler(int signum)
+{
+	exit(EXIT_SUCCESS);
+}
+//interrupt signal set up
+static void masterInterrupt(int seconds)
+{
+	//Invoke timer for termination
+	timer(seconds);
+
+	//Signal Handling for: SIGALRM
+	struct sigaction sa1;
+	sigemptyset(&sa1.sa_mask);
+	sa1.sa_handler = &masterHandler;
+	sa1.sa_flags = SA_RESTART;
+	if(sigaction(SIGALRM, &sa1, NULL) == -1)
+	{
+		perror("ERROR");
+	}
+
+	//Signal Handling for: SIGINT
+	struct sigaction sa2;
+	sigemptyset(&sa2.sa_mask);
+	sa2.sa_handler = &masterHandler;
+	sa2.sa_flags = SA_RESTART;
+	if(sigaction(SIGINT, &sa2, NULL) == -1)
+	{
+		perror("ERROR");
+	}
+
+	//Signal Handling for: SIGUSR1
+	signal(SIGUSR1, SIG_IGN);
+
+	//Signal Handling for: SIGSEGV
+	signal(SIGSEGV, segHandler);
+}
 
 static void initPCBT(ProcessControlBlock *pcbt)
 {
@@ -252,12 +328,19 @@ int main(int argc, char** argv){
 	
 	if(MAX_PROCESS > MAX_PROC)
 		MAX_PROCESS = MAX_PROC;
-	
+
+	unsigned char bitmap[MAX_PROCESS];
+
 	initOSS();
+
 	if(debugMode)
 	{
 		fprintf(stderr, "DEBUG mode is ON\n");
 	}
+
+
+	int last_index = -1;
+	
 	//main function
 	while(1)
 	{ 
@@ -339,8 +422,252 @@ int main(int argc, char** argv){
 				}
 			}
 		}
+
+
+		
+		incShmclock(0);
+
+		QNode qnext;
+		Queue *queueTrack = createQueue();
+
+		qnext.next = queue->front;
+		while(qnext.next != NULL)
+		{
+			incShmclock(0);
+
+			//sending a message to a specific child 
+			int c_index = qnext.next->index;
+			master_message.mtype = pcbt_shmptr[c_index].actualPid;
+			master_message.index = c_index;
+			master_message.childPid = pcbt_shmptr[c_index].actualPid;
+			int ossID = pcbt_shmptr[master_message.index].ossIndex;
+
+			msgsnd(mqueueid, &master_message, (sizeof(Message) - sizeof(long)), 0);
+
+
+			msgrcv(mqueueid, &master_message, (sizeof(Message) - sizeof(long)), 1, 0);
+
+
+			incShmclock(0);
+
+			//if child want to terminate, skips the current iteration of the loop and continues with the next iteration
+			if(master_message.flag == 0)
+			{
+				printWrite(fptr, "MASTER: process with PID %d has finish running at my time %d:%d\n",
+					ossID, shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+
+				//return all allocated frame from this process
+				int i;
+				for(i = 0; i < MAX_PAGE; i++)
+				{
+					if(pcbt_shmptr[c_index].page_table[i].frameNo != -1)
+					{
+						int frame = pcbt_shmptr[c_index].page_table[i].frameNo;
+						deleteListElement(refString, c_index, i, frame);
+						main_memory[frame / 8] &= ~(1 << (frame % 8));
+					}
+				}
+			}
+			else
+			{
+
+				total_access_time += incShmclock(0);
+				enQueue(queueTrack, c_index);
+
+				unsigned int address = master_message.address;
+				unsigned int request_page = master_message.requestPage;
+				int ossID = pcbt_shmptr[master_message.index].ossIndex;
+				if(pcbt_shmptr[c_index].page_table[request_page].ref == 0)
+				{
+ 					printWrite(fptr, "MASTER: process %d requesting read of address %d at time %d:%d\n", ossID, address, 
+						shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+				}
+				else
+				{
+					printWrite(fptr, "MASTER: process %d requesting write of address %d at time %d:%d\n", ossID, address, 
+						shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+				}
+				memoryaccess_number++;
+
+				//checking valid bit of the current page
+				if(pcbt_shmptr[c_index].page_table[request_page].valid == 0)
+				{
+ 					printWrite(fptr, "MASTER: address %d is not in a frame, pagefault\n", address);
+					pagefault_number++;
+
+
+					total_access_time += incShmclock(14000000); //adding time for reading and writing reference
+
+					//checking if main memory has open spot
+					bool is_memory_open = false;
+					int count_frame = 0;
+					while(1)
+					{
+						last_frame = (last_frame + 1) % MAX_FRAME;
+						uint32_t frame = main_memory[last_frame / 8] & (1 << (last_frame % 8));
+						if(frame == 0)
+						{
+							is_memory_open = true;
+							break;
+						}
+
+						if(count_frame >= MAX_FRAME - 1)
+						{
+							break;
+						}
+						count_frame++;
+					}
+
+
+					//continue if there are still space in the main memory
+					if(is_memory_open == true)
+					{
+						//allocate frame to this page and change the valid bit
+						pcbt_shmptr[c_index].page_table[request_page].frameNo = last_frame;
+						pcbt_shmptr[c_index].page_table[request_page].valid = 1;
+					
+						//Set the current frame to one 
+						main_memory[last_frame / 8] |= (1 << (last_frame % 8));
+
+						addListElement(refString, c_index, request_page, last_frame);
+						printWrite(fptr, "MASTER: allocated frame %d to PID %d\n", last_frame, ossID);
+
+					
+
+						//giving data to process or writing data to frame
+						if(pcbt_shmptr[c_index].page_table[request_page].ref == 0)
+						{
+							printWrite(fptr, "MASTER: address %d in frame %d, giving data to process %d at time %d:%d\n",
+								address, pcbt_shmptr[c_index].page_table[request_page].frameNo, ossID,
+								shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+
+							pcbt_shmptr[c_index].page_table[request_page].dirty = 0;
+						}
+						else
+						{
+							printWrite(fptr, "MASTER: address %d in frame %d, writing data to frame at time %d:%d\n",
+								address, pcbt_shmptr[c_index].page_table[request_page].frameNo,
+								shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+
+							pcbt_shmptr[c_index].page_table[request_page].dirty = 1;
+						}
+					}
+					else
+					{
+						//memory is already full, now calling fifo replacement
+ 						printWrite(fptr, "MASTER: address %d is not in a frame, memory is full. Invoking FIFO page replacement...\n", address);
+
+						//FIFO Algorithm
+						if(debugMode)
+						{
+							printWrite(fptr, getList(refString));
+						}
+
+						unsigned int fifo_index = refString->front->index;
+						unsigned int fifo_page = refString->front->page;
+						unsigned int fifo_address = fifo_page << 10;
+						unsigned int fifo_frame = refString->front->frame;
+
+						if(pcbt_shmptr[fifo_index].page_table[fifo_page].dirty == 1)
+						{
+							printWrite(fptr, "MASTER: address %d was modified. Modified information is written back to the disk\n", 
+								fifo_address);
+						}
+
+						pcbt_shmptr[fifo_index].page_table[fifo_page].frameNo = -1;
+						pcbt_shmptr[fifo_index].page_table[fifo_page].dirty = 0;
+						pcbt_shmptr[fifo_index].page_table[fifo_page].valid = 0;
+
+						pcbt_shmptr[c_index].page_table[request_page].frameNo = fifo_frame;
+						pcbt_shmptr[c_index].page_table[request_page].dirty = 0;
+						pcbt_shmptr[c_index].page_table[request_page].valid = 1;
+
+						//update reference string
+						deleteListFirst(refString);
+						addListElement(refString, c_index, request_page, fifo_frame);
+
+						if(debugMode)
+						{
+							printWrite(fptr, "After invoking FIFO algorithm...\n");
+							printWrite(fptr, getList(refString));
+						}
+
+
+						//modify dirty bit when requesting write of address
+						if(pcbt_shmptr[c_index].page_table[request_page].ref == 1)
+						{
+							printWrite(fptr, "MASTER: dirty bit of frame %d set, adding additional time to the clock\n", last_frame);
+							printWrite(fptr, "MASTER: indicating to process %d that writing reference has happend to address %d\n", 
+								ossID, address);
+							pcbt_shmptr[c_index].page_table[request_page].dirty = 1;
+						}
+					}
+				}
+				else
+				{
+
+					//giving data to process or writing data to frame
+					if(pcbt_shmptr[c_index].page_table[request_page].ref == 0)
+					{
+						printWrite(fptr, "MASTER: address %d is already in frame %d, giving data to process %d at time %d:%d\n",
+							address, pcbt_shmptr[c_index].page_table[request_page].frameNo,
+							ossID, shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+					}
+					else
+					{
+						printWrite(fptr, "MASTER: address %d is already in frame %d, writing data to frame at time %d:%d\n",
+							address, pcbt_shmptr[c_index].page_table[request_page].frameNo,
+							shmclock_shmptr->second, shmclock_shmptr->nanosecond);
+					}
+				}
+			}
+
+			//point to the next queue element
+			qnext.next = (qnext.next->next != NULL) ? qnext.next->next : NULL;
+
+			//reset master message
+			master_message.mtype = -1;
+			master_message.index = -1;
+			master_message.childPid = -1;
+			master_message.flag = -1;
+			master_message.requestPage = -1;
+		}
+
+
+		//Reassigned the current queue
+		while(!isQueueEmpty(queue))
+		{
+			deQueue(queue);
+		}
+		while(!isQueueEmpty(queueTrack))
+		{
+			int i = queueTrack->front->index;
+			enQueue(queue, i);
+			deQueue(queueTrack);
+		}
+		free(queueTrack);
+		
+		incShmclock(0);
+
+		//check if child has existed with no waiting 
+		int child_status = 0;
+		pid_t child_pid = waitpid(-1, &child_status, WNOHANG);
+
+		//set the return index bit back to zero
+		if(child_pid > 0)
+		{
+			int return_index = WEXITSTATUS(child_status);
+			bitmap[return_index / 8] &= ~(1 << (return_index % 8));
+		}
+
+		//end the infinite loop when reached maximum process
+		if(fork_number >= TOTAL_PROCESS)
+		{
+			timer(0);
+			masterHandler(0);
+		}
+		
 	}
 
-
-	return EXIT_SUCCESS;
+	return EXIT_SUCCESS; 
 }
